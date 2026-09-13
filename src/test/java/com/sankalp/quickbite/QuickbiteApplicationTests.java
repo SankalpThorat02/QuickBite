8609f511e2212b5aa836b227a44e3ea3cd4d4656
SankalpThorat02 <sankalpt2225@gmail.com>
2026-09-13 22:35:41 +0530

    Initial commit

---
+++ src/test/java/com/sankalp/quickbite/QuickbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.quickbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickbiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
